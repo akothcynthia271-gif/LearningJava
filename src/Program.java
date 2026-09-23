19e3e9a08e4af9ec48444b25a5dc849f7a378e0b
Cynthia <akothcynthia271@gmail.com>
2026-09-23 14:59:57 +0300

    print a message three times

---
+++ src/Program.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Program {
+    public void main(String [] args){
+        Scanner scanner= new Scanner(System.in);
+
+        System.out.println("Write something:");
+
+        string message= Scanner.nextLine();
+    }
+}
