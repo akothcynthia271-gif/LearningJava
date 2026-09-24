ad559c6e86f3a841c67c287189f4ea33df7f71d6
Cynthia <akothcynthia271@gmail.com>
2026-09-24 21:24:53 +0300

    print numbers

---
+++ src/Print.java
@@ -0,0 +1,21 @@
+public class Print {
+    public static void main(String [] args){
+        int chicken= 3;
+        double Bacon= 5.5;
+        String tractor = "none!";
+        System.out.println("Chicken:");
+        System.out.println(chicken);
+        System.out.println("Bacon(kg):");
+        System.out.println(Bacon);
+        System.out.println("Tractor:");
+        System.out.println(tractor);
+
+        System.out.println("And finally, a summary");
+        System.out.println(3);
+        System.out.println(5.5);
+        System.out.println("none");
+
+
+
+    }
+}
